d81688db7764ade91f50cc5bafafd20d5b928994
Dominic Arrojado <dominicarrojado@gmail.com>
2022-05-28 00:21:52 +0800

    fix input border radius on ios

--- components/inputBox.tsx
+++ components/inputBox.tsx
@@ -6,7 +6,7 @@ type Props = HTMLProps<HTMLInputElement> & {
 };
 
 export const inputBoxClassName = cn(
-  'block w-full border p-[12px] text-purple-450 text-[18px] leading-[1] tracking-[1px] text-ellipsis outline-none placeholder:text-purple-450 placeholder:opacity-60',
+  'block w-full border rounded-0 p-[12px] text-purple-450 text-[18px] leading-[1] tracking-[1px] text-ellipsis outline-none placeholder:text-purple-450 placeholder:opacity-60',
   'sm:text-[22px] sm:p-[17px]',
   'transition-colors focus:ring-[3px] focus:ring-black focus:border-black disabled:bg-gray-200',
   'placeholder:transition-opacity focus:placeholder:opacity-0'
