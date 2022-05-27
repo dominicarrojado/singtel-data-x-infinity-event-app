6c95a212c88d246390f7238860cda6b357878934
Dominic Arrojado <dominicarrojado@gmail.com>
2022-05-27 19:02:46 +0800

    implement tag manager

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import 'cropperjs/dist/cropper.css';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { useState } from 'react';
 import SeoTags from '../components/seoTags';
 import Layout from '../components/layout';
 import { Entry } from '../lib/types';
 import { StoreContext } from '../lib/store';
+import TagManager from '../components/tagManager';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [entries, setEntries] = useState<Array<Entry>>([]);
@@ -24,6 +26,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       }}
     >
       <SeoTags />
+      <TagManager />
       <Layout>
         <Component {...pageProps} />
       </Layout>
