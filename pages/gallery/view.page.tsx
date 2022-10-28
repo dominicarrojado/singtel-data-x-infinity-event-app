d597f0aa13db6dccc70c9b5cefa2b551c8731845
Dominic Arrojado <dominicarrojado@gmail.com>
2022-10-28 09:27:37 +0800

    remove indexing for gallery view page

--- pages/gallery/view.page.tsx
+++ pages/gallery/view.page.tsx
@@ -1,13 +1,11 @@
 import { useRouter } from 'next/router';
-import { useContext, useEffect, useMemo } from 'react';
+import { useContext, useEffect } from 'react';
 import { NextSeo } from 'next-seo';
-import { getRouteCanonical } from '../../lib/route';
 import EntryIndex from '../../components/entryIndex';
 import { Route } from '../../lib/types';
 import { StoreContext } from '../../lib/store';
 
 export default function GalleryView() {
-  const metaUrl = useMemo(() => getRouteCanonical(Route.GALLERY_VIEW), []);
   const router = useRouter();
   const { selectedEntry } = useContext(StoreContext);
 
@@ -21,7 +19,7 @@ export default function GalleryView() {
 
   return (
     <>
-      <NextSeo canonical={metaUrl} openGraph={{ url: metaUrl }} />
+      <NextSeo noindex />
       {selectedEntry ? <EntryIndex selectedEntry={selectedEntry} /> : null}
     </>
   );
